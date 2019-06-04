1ed7ffa7f5b504dedd2443a7c549d7c400c4fe45
Vivek Verma <vivnps.verma@gmail.com>
2019-06-04 15:25:54 -0700

    Objects on click

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+let objects = [];
+let G = 6.67e-11;
+
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-    background(220);
+    for (i = 0; i < objects.length; i++) {
+        draw(objects[i].obj);
+    }
+    background(0);
+}
+
+function mouseClicked() {
+    object = { obj: ellipse(mouseX, mouseY, 10, 10), mass: 10 }
+    objects.push(object);
+    return false;
 }
